ad830e0401f9ede15fc93a505561dd9c3b10f2fe
Tom Hutchings <goerofmuns@gmail.com>
2017-04-21 23:19:36 +0100

    fixed tabs and colours

--- TestRig.cpp
+++ TestRig.cpp
@@ -4,28 +4,29 @@
 
 int main()
 {
-	Panel panel;
-	CPU cpu;
-	cpu.init();
-	panel.init(&cpu);
-
-	initscr();
-	timeout(1);
+    Panel panel;
+    CPU cpu;
+    cpu.init();
+    panel.init(&cpu);
+    
+    initscr();
+    timeout(1);
     cbreak();
     noecho();
     intrflush(stdscr, false);
     keypad(stdscr, true);
 
-	start_color();
-
-	init_pair(1, COLOR_WHITE, COLOR_BLACK);
-	init_pair(2, COLOR_RED , COLOR_BLACK);
-	init_pair(3, COLOR_CYAN, COLOR_BLACK);
-
-	while(true)
-	{
-		panel.drawPanel(10, 4);
-		panel.update();
-	}
-	endwin();
+    start_color();
+    use_default_colors();
+    
+    init_pair(1, COLOR_WHITE, -1);
+    init_pair(2, COLOR_RED ,  -1);
+    init_pair(3, COLOR_CYAN,  -1);
+    
+    while(true)
+    {
+	panel.drawPanel(10, 4);
+	panel.update();
+    }
+    endwin();
 }
