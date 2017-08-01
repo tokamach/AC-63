9d34a166eb87c3c3953908b6123a87f3c0c9695e
Tom Hutchings <goerofmuns@gmail.com>
2017-08-01 02:13:45 +0100

    test assembling multiple instructions

--- Test.cpp
+++ Test.cpp
@@ -1,9 +1,12 @@
 #include <assert.h>
+#include <vector>
 #include <string>
 #include <iostream>
 
 #include "CPU.h"
 #include "asm.hpp"
+
+using std::vector;
 
 //
 // This file contains tests for all instructions
@@ -14,9 +17,21 @@ int main()
     CPU cpu;
     cpu.init();
 
-    //test make instruction
+    //test assembler
     assert(assemble(mkop("ADD", "", 0, 50)) == 0b101000000000110010);
     assert(assemble(mkop("ADD", "IZ", 0, 30)) == 0b101011000000011110);
+
+    vector<Operation> va = {
+	mkop("ADD", "", 0, 50),
+	mkop("ADD", "IZ", 0, 30),
+	mkop("ADD", "", 0, 50)
+    };
+    auto vr = assemble(va);
+
+    for (auto v : vr)
+    {
+	std::cout << std::bitset<18>(v) << std::endl;
+    }
 
     //DAM
     
