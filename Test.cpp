5f8f3f9e09514dfb0a9828d9da826c9db719097c
Tom Hutchings <goerofmuns@gmail.com>
2017-08-01 02:00:38 +0100

    move assembler to own file

--- Test.cpp
+++ Test.cpp
@@ -1,52 +1,13 @@
 #include <assert.h>
-#include <map>
-#include <utility>
 #include <string>
 #include <iostream>
 
 #include "CPU.h"
-
-using std::string;
-using std::map;
-using std::pair;
+#include "asm.hpp"
 
 //
 // This file contains tests for all instructions
 //
-
-//fucking love modern C++
-word makeInstruction(string op, string iz, byte reg, word arg)
-{
-    word result = 0;
-    map<string, byte> opmap = {
-	{"DAM",  0},
-	{"LDR",  1},
-	{"DPR",  2},
-	{"JMP",  3},
-	{"JEZ",  4},
-	{"JNZ",  5},
-	{"SHL",  6},
-	{"SHR",  7},
-	{"AND",  8},
-	{"OR" ,  9},
-	{"ADD", 10},
-	{"SUB", 11},
-	{"POP", 12},
-	{"PSH", 13}};
-
-    map<string, byte> indirect_page_map = {
-	{""  , 0},
-	{"Z" , 1},
-	{"I" , 2},
-	{"IZ", 3}};
-
-    result |= (opmap[op] << 14);
-    result |= (indirect_page_map[iz] << 12);
-    result |= (reg << 10);
-    result |= arg;
-
-    return result;
-}
 
 int main()
 {
@@ -54,8 +15,8 @@ int main()
     cpu.init();
 
     //test make instruction
-    assert(makeInstruction("ADD", "", 0, 50) == 0b101000000000110010);
-    assert(makeInstruction("ADD", "IZ", 0, 30) == 0b101011000000011110);
+    assert(assemble(mkop("ADD", "", 0, 50)) == 0b101000000000110010);
+    assert(assemble(mkop("ADD", "IZ", 0, 30)) == 0b101011000000011110);
 
     //DAM
     
---
+++ asm.hpp
@@ -0,0 +1,70 @@
+#include <vector>
+#include <map>
+#include <string>
+#include "CPU.h"
+
+using std::vector;
+using std::string;
+using std::map;
+
+struct Operation
+{
+    string op;
+    string iz;
+    byte reg;
+    word arg;
+};
+
+Operation mkop(string _op, string _iz, byte _reg, word _arg)
+{
+    Operation ret;
+    ret.op = _op;
+    ret.iz = _iz;
+    ret.reg = _reg;
+    ret.arg = _arg;
+    return ret;
+}
+
+word assemble(Operation op_arg)
+{
+    word result = 0;
+    map<string, byte> opmap = {
+	{"DAM",  0},
+	{"LDR",  1},
+	{"DPR",  2},
+	{"JMP",  3},
+	{"JEZ",  4},
+	{"JNZ",  5},
+	{"SHL",  6},
+	{"SHR",  7},
+	{"AND",  8},
+	{"OR" ,  9},
+	{"ADD", 10},
+	{"SUB", 11},
+	{"POP", 12},
+	{"PSH", 13}};
+    
+    map<string, byte> indirect_page_map = {
+	{""  , 0},
+	{"Z" , 1},
+	{"I" , 2},
+	{"IZ", 3}};
+    
+    result |= (opmap[op_arg.op] << 14);
+    result |= (indirect_page_map[op_arg.iz] << 12);
+    result |= (op_arg.reg << 10);
+    result |= op_arg.arg;
+    
+    return result;
+}
+
+vector<word> assemble(vector<Operation> op_arg)
+{
+    vector<word> ret;
+    for(auto &elem : op_arg)
+    {
+	ret.push_back(assemble(elem));
+    }
+
+    return ret;
+}
