08831a9a56762cdccf9cc6c587a986479b79b836
Tom Hutchings <goerofmuns@gmail.com>
2017-08-01 01:34:47 +0100

    add main

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <ncurses.h>
+#include <iostream>
+
+#include "CPU.h"
+#include "Panel.h"
+
+int main()
+{
+    Panel panel;
+    CPU cpu;
+    cpu.init();
+    panel.init(&cpu);
+    
+    initscr();
+    timeout(1);
+    cbreak();
+    noecho();
+    intrflush(stdscr, false);
+    keypad(stdscr, true);
+
+    start_color();
+    use_default_colors();
+    
+    init_pair(1, COLOR_WHITE, -1);
+    init_pair(2, COLOR_RED ,  -1);
+    init_pair(3, COLOR_CYAN,  -1);
+    
+    while(true)
+    {
+	panel.drawDebugInfo();
+	panel.drawPanel(10, 4);
+	panel.update();
+    }
+    endwin();
+}
